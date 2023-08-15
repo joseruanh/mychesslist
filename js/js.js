2a9f14cd659ae10dae3e75f7397433a1dd8fc521
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-08-15 12:43:12 -0300

    att

--- js/js.js
+++ js/js.js
@@ -18,8 +18,11 @@ function inglesa()
         <br>
 
         <a href="https://www.chess.com/pt-BR/article/view/aprenda-a-abertura-inglesa" target="_blank"> link chess.com - Krikor Sevag </a>
+        <br><br>
+        <div class="imagem"> <p> Botvinnik vs Sargon </p> <img class="gif" src="https://upload.wikimedia.org/wikipedia/commons/a/a9/Botvinnik_vs_Sargon%2C_1983.gif"> <p>1.c4e5 2.Nc3d6 3.g3Be6 4.Bg2Nc6 5.d3Nf6 6.f4Be7 7.Nf3O-O 8.O-OQd7 9.e4Bg4 10.h3Bxh3 11.f5Bxg2 12.Kxg2Nb4 13.a3Na6 14.b4c5 15.b5Nc7 16.Rh1a6 17.b6Nce8 18.Ng5Qc6 19.Rb1Bd8 20.Nd5h6 21.Nf3Nxd5 22.exd5Qd7 23.g4a5 24.Nd2Ra6 25.Ne4Rxb6 26.Rxb6Bxb6 27.f6Nxf6 28.Nxf6+gxf6 29.Bxh6Re8 30.Qf3Bd8 31.Qh3Qa4 32.Bd2Kf8 33.Rf1Kg8 34.Qh6Qd7 35.Kg3f5 36.Rh1f4+ 37.Kf3 1-0</p> </div>
+        
     
-    </div>
+        <br><br><br><br><br><br><br><br><br></div>
 
     <div class="video">
 
