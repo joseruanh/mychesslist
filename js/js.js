44655ac2a8fc06bd026fdebd955f519b06997c4b
Ruanh <79330021+joseruanh@users.noreply.github.com>
2023-08-15 12:14:50 -0300

    adicionando site

---
+++ js/js.js
@@ -0,0 +1,65 @@
+function mychesslist()
+{
+    location.reload()
+}
+
+function inglesa()
+{
+    let a = window.document.getElementById("res")
+    a.innerHTML = `
+    
+    <div class="conteudo">
+
+        <p class="titulo">A Abertura Inglesa</p> <br>
+        <p>A abertura inglesa é uma abertura pra quem ama um jogo mais posicinal.</p> <br>
+        <p>1.c4!, a conhecida Abertura Inglesa, que leva esse nome por causa do enxadrista britânico Howard Staunton, considerado como o melhor jogador do mundo no começo do século 19. Ele a utilizou repetidas vezes em seu match contra Saint Amant em 1843.</p>
+        <br>
+        <p>Como mencionado no livro, uma das principais vantagens da Inglesa é manter a flexibilidade, e poder jogar eventualmente com g3, Cc3 seguido de e4, ou mesmo d4 em algum momento, transpondo para aberturas estilo peão dama. Abaixo vou colocar algumas partidas com vitórias interessantes das brancas, com os mais variados planos:</p>
+        <br>
+
+        <a href="https://www.chess.com/pt-BR/article/view/aprenda-a-abertura-inglesa" target="_blank"> link chess.com - Krikor Sevag </a>
+    
+    </div>
+
+    <div class="video">
+
+    <p class="titulo">Aprenda a Abertura Inglesa - Evandro Barbosa</p>
+    <div><iframe src="https://www.youtube.com/embed/_zNWTebKDFM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+
+    <br><br>
+
+    <p class="titulo"> Aprenda a Abertura Inglesa - Krikor Sevag </p>
+    <div><iframe src="https://www.youtube.com/embed/SXg7YrPyZYY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <br></div>
+
+    <p class="titulo"> Abertura Inglesa | Ideias Gerais - Xadrez Nobre</p>
+    <div><iframe src="https://www.youtube.com/embed/jytlgKrOUwo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+    <br>
+
+    <p class="titulo"> Esse é o futuro do xadrez? - Krikor Sevag </p>
+    <div><iframe src="https://www.youtube.com/embed/BEW8keCzFu8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+    
+    
+    <br><br><br><br><br><br><br><br>
+    </div>
+
+ 
+    
+    
+    
+    
+    
+    `
+}
+function estrategia()
+{
+    window.alert("estrategia")
+}
+function finais()
+{
+    window.alert("finais")
+}
+function videos()
+{
+    window.alert("videos")
+}
